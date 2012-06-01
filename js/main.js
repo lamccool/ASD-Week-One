1d05cca6879ae5424f3d16d984ba588b1468ff17
Laura McCool <mccool_laura@yahoo.com>
2012-05-31 19:45:57 -0500

    updated my js to jquery

--- js/main.js
+++ js/main.js
@@ -4,41 +4,35 @@
 
 var parseGiftForm = function(data){};
 
-$(document).ready(function(){
-		
+$('#home').live('pageinit',function(){
+
 	var rbform = $('#giftForm');	
 	rbform.validate({
 		invalidHandler: function(form, validator){},
 		submitHandler: function(){
-			var data = rbform.serializeArray();
+			var data = $('rbform').serializeArray();
 			parseGiftForm(data);
 		}
 	});
 
-});
+$('#window').bind("DOMContentLoaded", function(){
 
-
-/*window.addEventListener ("DOMContentLoaded", function(){
-	function $(x){
-		var theElement = document.getElementById(x);
-		return theElement;
-	}
 	//create select field element and populate it with options.
 	function makeCats (){
 		var formTag = document.getElementsByTagName("form"), //formTag is an array of all form tags.
 			selectLi = $('select'),
-			makeSelect = document.createElement('select');
-			makeSelect.setAttribute("id", "category");
+			makeSelect = $("select"); 
+			$('#makeSelect').attr("id", "category");
 		for (var i=0, j=giftCategory.length; i<j; i++){
-			 var makeOption = document.createElement('option');
+			 var makeOption = $('option');
 			 var optText = giftCategory[i];
-			 makeOption.setAttribute("value", optText);
+			 $('#makeOption').attr("value", optText);
 			 makeOption.innerHTML = optText;
-			 makeSelect.appendChild(makeOption);
+			 $('makeSelect').append('makeOption');
 		}
 		//selectLi.appendChild(makeSelect);
 	}
-	
+
 	//Find value of selected radio button
 	function getSelectedRadio(){
 		var radios = document.forms(0).location;
@@ -48,27 +42,27 @@ $(document).ready(function(){
 			}
 		}
 	}
-	
+
 	function toggleControls(n){
 		switch(n){
 			case "on":
-				$('giftForm').style.display ="none";
-				$('clear').style.display ="inline";
-				$('displayData').style.display ="none";
-				$('addNew').style.display = "inline";
+				$('giftForm').hide();
+				$('clear').css('display','inline');
+				$('displayData').hide();
+				$('addNew').css('display','inline');
 				break;
 			case "off":
-				$('giftForm').style.display ="block";
-				$('clear').style.display ="inline";
-				$('displayData').style.display ="inline";
-				$('addNew').style.display = "none";
-				$('items').style.display = "none";
+				$('giftForm').show();
+				$('clear').css('display', 'inline');
+				$('displayData').css('display', 'inline');
+				$('addNew').hide();
+				$('items').hide();
 				break;
 			default:
 				return false;
 		}
 	}
-	
+
 	function storeData(key){
 	// If there is no key this means this is a brand new item and we need a new key
 		if (!key){
@@ -90,7 +84,7 @@ $(document).ready(function(){
 			item.store 	 		= ["Store Name:", $('store').value];
 			item.url	 	 	= ["Product Url:", $('url').value];
 			item.date	 	 	= ["Date Added:", $('date').value];
-		
+
 		//Save Data in localStorage: Use Stringify to convert our object to a string.
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Item Added!");
@@ -103,66 +97,68 @@ $(document).ready(function(){
 			alert("There is no data in Local Storage so default data was added.");
 			autoFillData();
 		}
-		var makeDiv = document.createElement('div');
-		makeDiv.setAttribute("id", "items");
-		var makeList = document.createElement('ul');
-		makeDiv.appendChild(makeList);
-		document.body.appendChild(makeDiv);
-		$('items').style.display = "block";
+		var makeDiv = $('div');
+		$('#makeDiv').attr("id", "items");
+		var makeList = $('ul');
+		$('makeDiv').append('makeList');
+		$('document.body').append('makeDiv');
+		$('items').show();
 		for(var i=0, len=localStorage.length; i<len;i++){
-			var makeli = document.createElement('li');
-			var linksLi = document.createElement ('li');
-			makeList.appendChild(makeli);
+			var makeli = $('li');
+			var linksLi = $('li');
+			$('makeList').append('makeli');
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			//Convert string from local storage value back to an object by using JSON.parse
 			var obj = JSON.parse(value);
-			var makeSubList = document.createElement('ul');
-			makeli.appendChild(makeSubList);
+			var makeSubList = $('ul');
+			$('makeli').append('makeSubList');
 			getImage(obj.category[1], makeSubList);
 			for(var n in obj){
-				var makeSubli = document.createElement('li');
-				makeSubList.appendChild(makeSubli);
+				var makeSubli = $('li');
+				$('makeSubList').append('makeSubli');
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubli.innerHTML = optSubText;
-				makeSubli.appendChild (linksLi);
+				$('makeSubli').append('linksLi');
 			}
 			makeItemLinks(localStorage.key(i), linksLi); //Edit and delete buttons for Local Storage
 		}
 	}
 	//Get image for the right category
 	function getImage(catName, makeSubList){
-		var imageLi = document.createElement('li');
-		makeSubList.appendChild(imageLi);
-		var newImg = document.createElement('img');
-		var setSrc = newImg.setAttribute("src","images/"+ catName +".png");
-		imageLi.appendChild(newImg);
+		var imageLi = $('li');
+		$('makeSubList').append('imageLi');
+		var newImg = $('img');
+		var setSrc = $('#newImg').attr("src","images/"+ catName +".png");  
+		$('imageLi').append('newImg');
 	}
 	//Make Item Links Function
 	//Create edit and delete links for each stored item when displayed
-	
+
 	function makeItemLinks (key, linksLi){
 	//add edit single item link
-		var editLink = document.createElement ('a');
+		var editLink = $('a');
 		editLink.href = "#";
 		editLink.key = key;
 		var editText ="Edit List";
-		editLink.addEventListener("click", editItem);
+		$('#editLink').bind("click", editItem);
+		//editLink.addEventListener("click", editItem);
 		editLink.innerHTML = editText;
-		linksLi.appendChild (editLink);
-		
+		$('linksLi').append('editLink');
+
 		//add line break
-		var breakTag = document.createElement('br');
-		linksLi.appendChild(breakTag);
-		
+		var breakTag = $('br');
+		$('linksLi').append('breakTag');
+
 		//delete single item link
-		var deleteLink = document.createElement('a');
+		var deleteLink = $('a');
 		deleteLink.href = "#";
 		deleteLink.key = key;
 		var deleteText = "Delete Item"
-		deleteLink.addEventListener("click", deleteItem);
+		$('#deleteLink').bind("click", deleteItem);
+		//deleteLink.addEventListener("click", deleteItem);
 		deleteLink.innerHTML = deleteText;
-		linksLi.appendChild (deleteLink);
+		$('linksLi').append('deleteLink');
 	}
 	//Auto Populate Local Storage
 	function autoFillData(){
@@ -170,19 +166,19 @@ $(document).ready(function(){
 			var id = Math.floor(Math.random()*100000001);
 			localStorage.setItem(id,JSON.stringify(json[n]));
 		}
-	
-	}
-	
 
-	
+	}
+
+
+
 	function editItem (){
 		//grab date from local storage
 		var value = localStorage.getItem(this.key);
 		var item = JSON.parse(value);
-		
+
 		//show form
 		toggleControls("off");
-		
+
 		//populate form fields with current local storage values
 		$('category').value = item.category[1];
 		$('comments').value = item.comments[1];
@@ -199,7 +195,7 @@ $(document).ready(function(){
 		$('store').value = item.store[1];
 		$('url').value = item.url[1];
 		$('date').value = item.date[1];
-		
+
 		//remove the initial listener from input 'save contact' button
 		save.removeEventListener ("click", storeData);
 		//Change submit button value to say edit button
@@ -210,7 +206,7 @@ $(document).ready(function(){
 		editSubmit.addEventListener("click", validate);
 		editSubmit.key = this.key;		
 	}
-	
+
 	function deleteItem(){
 		var ask = confirm("Are you sure you want to delete this item?")
 		if (ask){
@@ -221,7 +217,7 @@ $(document).ready(function(){
 			alert("Item was NOT deleted.")
 		}
 	}
-	
+
 	function clearLocal(){
 		if(localStorage.length === 0){
 			alert("There is no data to clear.")
@@ -232,41 +228,41 @@ $(document).ready(function(){
 			return false;	
 		}
 	}
-	
+
 	function validate(e){
 		//define elements we want to check
 		var getCategory = $('category');
 		var getComments = $('comments');
 		var getAmount = $('amount');
-		
+
 		//Reset Error Message
 		errMsg.innerHTML ="";
-		getCategory.style.border = "1px solid black";
-		getComments.style.border = "1px solid black";
-		getAmount.style.border = "1px solid black";
-		
+		$('#getCategory').css({border: '1px solid black'});
+		$('#getComments').css({border: '1px solid black'});
+		$('#getAmount').css({border: '1px solid black'});
+
 		//Get Error Messages
 		var messageAry = [];
 		//Category Validation
 		if (getCategory.value === "--Choose A Gift Category--"){
 			var categoryError = "Please choose a category.";
-			getCategory.style.border = "1px solid red";
+			$('#getCategory').css({border: '1px solid red'});
 			messageAry.push(categoryError);
 		}
-		
+
 		//Comments validation
 		if (getComments.value === ""){
 			var commentsError = "Please include your item.";
-			getComments.style.border = "1px solid red";
+			$('#getComments').css({border: '1px solid red'});
 			messageAry.push(commentsError);
 		}
-		
+
 		//If there were errors display them on the screen
 		if(messageAry.length >= 1){
 			for(var i=0, j=messageAry.length; i < j; i++){
-				var txt = document.createElement('li');
+				var txt = $('li');
 				txt.innerHTML = messageAry[i];
-				errMsg.appendChild(txt);
+				$('errMsg').append('txt');
 			}	
 			e.preventDefault();
 			return false;	
@@ -276,24 +272,19 @@ $(document).ready(function(){
 			storeData(this.key);
 		}
 
-	}
-	
+	};
+
 	//var defaults
 	var giftCategory = ["--Choose A Gift Category--", "Electronics", "Entertainment", "Apparel", "Music", "Books", "Etc"],
-		locationValue
+		locationValue,
 		errMsg = $('errors');
-	;
 	makeCats();
 	//set link and click events
 	var displayData = $('displayData');
-	displayData.addEventListener("click", getData);
+	$('#displayData').bind("click", getData);
 	var clearData =$('clear');
-	clearData.addEventListener("click", clearLocal);
+	$('#clearData').bind("click", clearLocal);
 	var save = $('submit');
-	save.addEventListener("click", validate);
+	$('#save').bind("click", validate);
 
-});*/
-
-
-
-
+});
